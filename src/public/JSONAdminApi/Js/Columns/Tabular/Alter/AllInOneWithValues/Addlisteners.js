45701e05330b46ec6bb10235cf1fd5bdeb78dcb2
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2022-12-19 20:13:25 +0530

    SREENIVAS

--- src/public/JSONAdminApi/Js/Columns/Tabular/Alter/AllInOneWithValues/Addlisteners.js
+++ src/public/JSONAdminApi/Js/Columns/Tabular/Alter/AllInOneWithValues/Addlisteners.js
@@ -47,7 +47,14 @@ let jFLocalClickFunc = async (event) => {
 
     switch (response.status) {
         case 200:
-            window.location = "";
+            let jVarLocalNewLocation = "";
+            jVarLocalNewLocation += `?inFolderName=${jVarLocalFolderName}`
+            jVarLocalNewLocation += `&inFileName=${jVarLocalfilename}`
+            jVarLocalNewLocation += `&inItemName=${jVarLocalitemname}`
+            jVarLocalNewLocation += `&inScreenName=${jVarLocalscreenname}`
+            jVarLocalNewLocation += `&inColumnName=${jVarLocalDataAttributeValue}`;
+            console.log("jVarLocalNewLocation : ", jVarLocalNewLocation);
+            window.location = jVarLocalNewLocation;
             break;
         case 204:
             Swal.fire('Not Update Data')
