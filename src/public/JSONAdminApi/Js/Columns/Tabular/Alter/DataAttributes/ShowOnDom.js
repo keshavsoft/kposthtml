0e836e19e01f1f567f90a4650e6b771da6408ce3
JatinNeeraj <jatinucm123@gmail.com>
2022-12-23 17:44:41 +0530

    SREENIVAS

--- src/public/JSONAdminApi/Js/Columns/Tabular/Alter/DataAttributes/ShowOnDom.js
+++ src/public/JSONAdminApi/Js/Columns/Tabular/Alter/DataAttributes/ShowOnDom.js
@@ -1,16 +1,13 @@
-import { jFStartFunc as ApplyClassesjFStartFunc } from "./ApplyClasses.js";
+import { jFStartFunc as DataAttributesjFStartFunc } from "./ApplyClasses.js";
 
 let jFCreateFoldersToDom = async () => {
     let jVarLocalRoute = window.location.pathname.split("/")[1];
-    // let jVarLocalFetchUrl = `/${jVarLocalRoute}/AdminApi/Utility/Json/Folder/ForTableColumns/FromKeys/Toggles`;
-    let jVarLocalFetchUrl = `/${jVarLocalRoute}/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/SubKeys/ParentClasses`;
-
+    let jVarLocalFetchUrl = `/${jVarLocalRoute}/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/DataAttributes`;
     let jVarLocalFromFetch = await fetch(jVarLocalFetchUrl);
     let dataFromApi = await jVarLocalFromFetch.json();
-    console.log("dataFromApi",dataFromApi);
 
     if (dataFromApi !== null) {
-        ApplyClassesjFStartFunc({ inDataFromApi: dataFromApi });
+        DataAttributesjFStartFunc({ inDataFromApi: dataFromApi });
 
         let jVarLocalRawTemplate = document.getElementById("HbsTemplateForFoldersOnly").innerHTML;
         document.getElementById("KCont1").innerHTML = Handlebars.compile(jVarLocalRawTemplate)(dataFromApi);
