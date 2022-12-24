2593a8e2cb6e39ed65bd5e9d7459bdaa6ed60916
keshavsoft <nknnkumar@yahoo.com>
2022-12-24 11:57:45 +0530

    keshav

--- src/public/JSONReports/Js/Reports/Header/FirmDetailsToLocalStorage.js
+++ src/public/JSONReports/Js/Reports/Header/FirmDetailsToLocalStorage.js
@@ -1,14 +1,10 @@
 let FetchFunc = async (inEvent) => {
     let jVarLocalCurrentTarget = inEvent.currentTarget;
 
-    let jVarClosestInputGroup = jVarLocalCurrentTarget.closest(".input-group");
-
     let jVarLocalRoute = jVarGlobalProject;
     let jVarLocalSubRoute = jVarGlobalSubRoute;
 
-    let jVarLocalReportSelected = jVarClosestInputGroup.querySelector(".ReportSelectedClass").value;
-
-    let jVarLocalFetchUrl = `/${jVarLocalRoute}/${jVarLocalSubRoute}/Data/FromFolder/FirmDetails/ForReport`;
+    let jVarLocalFetchUrl = `/JSONApi/${jVarLocalSubRoute}/Data/FromFolder/FirmDetails/ForReport`;
     let jVarLocalResult = await fetch(jVarLocalFetchUrl);
     let jVarLocalResponse = await jVarLocalResult.json();
     console.log("aaaaaaa : ", jVarLocalResponse);
