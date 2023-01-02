9a816e570f3706fd6b01863e02c5a32d6f082bee
keshavsoft <nknnkumar@yahoo.com>
2023-01-02 20:56:55 +0530

    keshav

--- src/public/JSONAdminApi/Js/Columns/Tabular/Alter/Toggles.js
+++ src/public/JSONAdminApi/Js/Columns/Tabular/Alter/Toggles.js
@@ -2,7 +2,5 @@ import { jFUpdateFunc } from "./Toggles/Addlisteners.js";
 import { jFCreateFoldersToDom } from "./Toggles/ShowOnDom.js";
 
 jFCreateFoldersToDom().then(FromjFCreateFoldersToDom => {
-   // console.log("FromjFCreateFoldersToDom : ", FromjFCreateFoldersToDom);
-    
     jFUpdateFunc();
 });
