aff04f8452ab731990b0d309dd31feee9b969b7a
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-01-12 11:27:28 +0530

    SREENIVAS

--- src/public/JSONAdminApi/Js/SubColumns/Tabular/Alter/SubMenu/DisplayType/ShowOnDom.js
+++ src/public/JSONAdminApi/Js/SubColumns/Tabular/Alter/SubMenu/DisplayType/ShowOnDom.js
@@ -3,7 +3,7 @@ import { jFStartFunc as ApplyClassesjFStartFunc } from "./ApplyClasses.js";
 let jFCreateFoldersToDom = async () => {
     let jVarLocalRoute = window.location.pathname.split("/")[1];
     // let jVarLocalFetchUrl = `/${jVarLocalRoute}/AdminApi/Utility/Json/Folder/ForTableColumns/FromKeys/Toggles`;
-    let jVarLocalFetchUrl = `/${jVarLocalRoute}/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/SubKeys/DisplayType`;
+    let jVarLocalFetchUrl = `/${jVarLocalRoute}/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableColumns/SubKeys/DisplayType`;
 
     let jVarLocalFromFetch = await fetch(jVarLocalFetchUrl);
     let dataFromApi = await jVarLocalFromFetch.json();
